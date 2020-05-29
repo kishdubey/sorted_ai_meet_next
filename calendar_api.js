c5737a5984c511d3eb51493098afd0b591a2053b
Kish Dubey <dkish2000@gmail.com>
2020-05-29 00:17:38 -0400

    removed

--- calendar_api.js
+++
@@ -1,4 +0,0 @@
-let script = document.createElement('script');
-script.src = "https://apis.google.com/js/api.js"
-script.onload= function(){};handleClientLoad();
-script.onreadystatechange=if (this.readyState === 'complete') this.onload();
