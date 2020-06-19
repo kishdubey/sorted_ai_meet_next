0108eef50b062014af7c36c257d9afa2b40bdc3c
Kish Dubey <dkish2000@gmail.com>
2020-06-19 15:46:28 -0400

    small changes

--- background.js
+++ background.js
@@ -36,7 +36,7 @@ function onGAPILoad() {
 
 chrome.browserAction.onClicked.addListener(function() {
   // HTML rerouting
-  var meetings = "noMeetings.html";
+  var meetings = "meetings.html";
   var noMeetings = "noMeetings.html";
 
   chrome.storage.sync.get(['events'], function(result) {
